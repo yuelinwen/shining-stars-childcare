3bad0fdaf04172d04126ac6120b7cd7838f2e6da
yuelin <tayloryuelinwen@gmail.com>
2022-03-23 15:34:48 -0400

    update

---
+++ src/components/navbar/logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShiningStars from "../../assets/image/ShiningStarsHome-300x43.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={ShiningStars} alt="shining-stars-logo" />
+    </div>
+  );
+};
+
+export default Logo;
