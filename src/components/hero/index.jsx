0cd80fbded9d3f03afc0005b5d819444e2809f40
yuelin <tyleryuelinwen@gmail.com>
2022-03-24 00:54:46 -0400

    hero section

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./styles.scss";
 import heroImg from "../../assets/image/rs=w_1534,m.webp";
+import { Button } from "react-bootstrap";
 
 const HeroComponent = () => {
   return (
-    <div className="hero-container">
-      <div className="hero-img">
+    <>
+      <div className="hero-container">
         <img src={heroImg} alt="" />
-      </div>
 
-      <div className="hero-text">
-        <h1>Accepting 2022-2023 term</h1>
-        <button>REGISTER NOW</button>
-        <span>admin@email.com</span>
+        <div className="hero-text">
+          <h1>Accepting 2022-2023 term</h1>
+          <Button variant="danger" size="lg">
+            REGISTER NOW
+          </Button>
+          <h4>admin@email.com</h4>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
