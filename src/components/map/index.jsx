601303bf5cab99f41ae81e48f28d789c1adc2a0b
yuelin <tyleryuelinwen@gmail.com>
2022-03-24 22:46:50 -0400

    add demo google map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles.scss";
-
+import GoogleMapImg from "../../assets/image/WechatIMG271.jpeg";
 
 const MapComponent = () => {
   return (
     <div className="map-container">
       <h1>Our Location</h1>
-
+      <img src={GoogleMapImg} alt="demo-google-map" />
     </div>
   );
 };
