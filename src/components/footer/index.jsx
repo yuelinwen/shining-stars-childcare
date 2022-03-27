7d1d9904ce7b27e9a0750f3673a6d71e3f5ed720
yuelin <tyleryuelinwen@gmail.com>
2022-03-27 18:07:21 -0400

    update

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,17 +5,20 @@ const FooterComponent = () => {
   return (
     <div className="footer-container">
       <div className="footer-info">
-        <a href="#">
+        <a href="https://www.instagram.com/">
           <FaInstagram />
         </a>
-        <a href="#">
+        <a href="https://www.youtube.com/">
           <FaYoutube />
         </a>
-        <a href="#">
+        <a href="https://www.facebook.com/">
           <FaFacebook />
         </a>
       </div>
-      <p className="text"> © 2022 Shining Stars Child Care Centre. All rights reserved.</p>
+      <p className="text">
+        {" "}
+        © 2022 Shining Stars Child Care Centre. All rights reserved.
+      </p>
     </div>
   );
 };
